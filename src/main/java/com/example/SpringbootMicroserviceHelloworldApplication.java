35e4c6c8545ff48941b4900267e5086e247bbd51
blue-dio <dvasu84@outlook.com>
2026-02-12 20:57:34 +0530

    Create Spring Boot main application class

---
+++ src/main/java/com/example/SpringbootMicroserviceHelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMicroserviceHelloworldApplication.class, args);
+    }
+}
